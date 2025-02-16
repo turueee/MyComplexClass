c982beb5f6a4de7ac95ce82acc9faab194c5a703
turueee <turushkine@inbox.ru>
2025-02-16 22:03:46 +0300

    'f'

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -4,14 +4,40 @@ using namespace std;
 
 class MyComplex
 {
-public:
+private:
   double re{};
   double im{};
-
+public:
+  //Ввод и вывод
+  friend istream& operator>>(istream& o, MyComplex& b);
+  friend ostream& operator<<(ostream& o, MyComplex b);
+  //Арифметические операции
   MyComplex& operator+(const MyComplex& d);
   MyComplex& operator-(const MyComplex& d);
   MyComplex& operator*(const MyComplex& d);
+  MyComplex& operator/(const MyComplex& d);
 
+
+  //Геттеры
+  double get_re()
+  {
+    return this->re;
+  }
+  double get_im()
+  {
+    return this->im;
+  }
+  //Сеттеры
+  void set_re(double a)
+  {
+    this->re = a;
+  }
+  void set_im(double a)
+  {
+    this->im = a;
+  }
+  //Сравнение
+  friend bool operator==(const MyComplex a,const MyComplex b);
 };
 
 MyComplex& MyComplex::operator+(const MyComplex& d)
@@ -30,9 +56,27 @@ MyComplex& MyComplex::operator-(const MyComplex& d)
   return c;
 }
 
+MyComplex& MyComplex::operator*(const MyComplex& d)
+{
+  MyComplex c;
+  c.re = re * d.re - im * d.im;
+  c.im = re * d.im + im * d.re;
+  return c;
+}
+
+MyComplex& MyComplex::operator/(const MyComplex& d)
+{
+  MyComplex c;
+  c.re = (re * d.re + im * d.im) / (d.re * d.re + d.im * d.im);
+  c.im = (im * d.re - re * d.im) / (d.re * d.re + d.im * d.im);
+  return c;
+}
+
+
+
 ostream& operator<<(ostream& o, MyComplex b)
 {
-  o << b.re << "+i" << b.im;
+  o << b.re << "+" << b.im << "i";
   return o;
 }
 
@@ -43,10 +87,15 @@ istream& operator>>(istream& o, MyComplex& b)
   return o;
 }
 
+bool operator==(const MyComplex a,const MyComplex b)
+{
+  return (a.re==b.re && a.im==b.im)
+}
 
 void main()
 {
-  MyComplex c;
+  MyComplex c,f;
   cin >> c;
-  cout << c-c;
+  cin >> f;
+  cout << c==f;
 }
